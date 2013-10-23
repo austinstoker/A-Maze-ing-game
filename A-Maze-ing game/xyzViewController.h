9dc830a9ca447060f1573a6f52405efc6816f423
GWStoker <mac@Mac-5.local>
2013-10-23 08:45:50 -0600

    some bugs need fixing

--- A-Maze-ing game/xyzViewController.h
+++ A-Maze-ing game/xyzViewController.h
@@ -10,4 +10,30 @@
 
 @interface xyzViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIImageView *pacman;
+@property (strong, nonatomic) IBOutlet UIImageView *ghost2;
+@property (strong, nonatomic) IBOutlet UIImageView *ghost1;
+@property (strong, nonatomic) IBOutlet UIImageView *ghost3;
+@property (strong, nonatomic) IBOutlet UIImageView *exit;
+
+
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *wall;
+
+@property (assign, nonatomic) CGPoint currentPoint;
+@property (assign, nonatomic) CGPoint previousPoint;
+@property (assign, nonatomic) CGFloat pacmanXVelocity;
+@property (assign, nonatomic) CGFloat pacmanYVelocity;
+@property (assign, nonatomic) CGFloat angle;
+@property (assign, nonatomic) UIAcceleration *acceleration;
+//@property (strong, nonatomic) CMMotionManager  *motionManager;
+@property (strong, nonatomic) NSOperationQueue *queue;
+@property (strong, nonatomic) NSDate *lastUpdateTime;
+
+#import <CoreMotion/CoreMotion.h>
+
+#define kUpdateInterval (1.0f / 60.0f)
+
+#import <QuartzCore/CAAnimation.h>//;
+
 @end
+
